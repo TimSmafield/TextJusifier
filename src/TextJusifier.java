03062911c0fe8695d4c2a4c875499a272ee32fab
TimSmafield <timothysmafield@gmail.com>
2022-03-26 15:11:21 -0600

    initial commit

---
+++ src/TextJusifier.java
@@ -0,0 +1,13 @@
+public class TextJusifier {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public String justify( String line, Integer length ) {
+        
+        return null;
+    }
+
+}
